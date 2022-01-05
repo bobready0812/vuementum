6b984480ec697c16b8d719aef676cd11b034f78d
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-05 12:21:04 +0900

    20220105

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
+<div class="menu">
+  <a v-for="작명 in menus" :key="작명">{{작명}}</a>
+</div>
+
   <div>
+    원룸샵
     <h4> XX 원룸 </h4>
     <p> {{ price1 }} 만원 </p>
   </div>
@@ -18,6 +23,8 @@ export default {
     return {
       price1 : 60,
       price2 : 70,
+      menus : ['Home', 'Shop', 'About'],
+      products  : ['역삼동 원룸', '천호동 원룸', '마포구 원룸']
     }
   },
   components: {
